8c771eb757982e48c3c757db068c1df577a7086d
Madu Almeida <dudacsantos450@gmail.com>
2025-08-18 10:40:47 -0300

    Create condicao.py

---
+++ codigos/condicao.py
@@ -0,0 +1,26 @@
+faturamento = 1000
+custo = 1100
+
+lucro = faturamento - custo
+
+''' If/else if/elsif no PYTHON funciona por identação, tenha cuidado!
+if lucro >= 0:
+    print(f"Seu lucro foi de R${lucro: .2f}")
+else:
+    print(f"Prejuízo de R${lucro: .2f}")
+'''
+
+''' # Cadastrando produto novo na lista
+produtos = ["iphone" , "ipad" , "airpod"]
+novo_produto = input("Digite aqui o produto: ")
+novo_produto = novo_produto.lower() # Tratando informação do usuário, transformando sempre em letra minúscula
+
+if novo_produto in produtos:
+    print("Produto já cadastrado!")
+else:
+    print("Produto cadastrado com sucesso!")
+    produtos.append(novo_produto)
+
+print(produtos)
+'''
+

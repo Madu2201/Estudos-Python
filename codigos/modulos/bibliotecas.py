ecb9cecfa51da36030f23bb16c193d2392fc1501
Madu Almeida <dudacsantos450@gmail.com>
2025-08-23 19:59:03 -0300

    Update

--- codigos/modulos/bibliotecas.py
+++ codigos/modulos/bibliotecas.py
@@ -4,4 +4,4 @@ import os # Biblioteca os = Sistema operacional, permite você acessar as coisas
 arquivos = os.listdir()
 print(arquivos) # ['.git', '.gitattributes', 'codigos', 'exercicios', 'LICENSE', 'README.md']
 
-os.rename("ex011.py", "exercicios/ex011.py") # Renomear o arquivo, e também muda-lo de lugar
+os.rename("codigos/modulos/ex011.py", "exercicios/ex011.py") # Renomear o arquivo, e também muda-lo de lugar
